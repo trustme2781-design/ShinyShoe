42985cfa64f3e50178481724a893c165f1d9da40
trustme2781-design <trustme2781@gmail.com>
2025-12-17 14:31:14 +0530

    Update supabaseClient.ts

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://hqfjzmjqziiqooqxkcqp.supabase.co';
 // Note: In a production environment, this key should be an environment variable.
-const supabaseKey = 'sb_publishable__0ucpSU1mQnYeAovaeOK6g_dnfrYy5j';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhxZmp6bWpxemlpcW9vcXhrY3FwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5NDk5MzQsImV4cCI6MjA4MTUyNTkzNH0._4_9WXO86YisHPgHVqulN-x57nKC_7FkS2GR1fx5hjg';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
